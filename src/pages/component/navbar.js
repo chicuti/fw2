d5e9532256f0a5b92d8ad133fd67906b3fac3140
chicuti <142831353+chicuti@users.noreply.github.com>
2024-10-14 15:39:48 +0000

    aula dia 14-10

--- src/pages/component/navbar.js
+++ src/pages/component/navbar.js
@@ -1,3 +1,20 @@
-export default function Navbar() {
-    return <div>Menu</div>
+import {Container,Nav, Navbar} from 'react-bootstrap';
+import style from '@/styles/Navbar.module.css';
+
+export default function Menu() {
+  return (
+    <Container>
+    <Navbar expand="sm" className={`${style.navGradiente} my-3 px-2 border border-2 border-success rounded rounded-pill-3`} >
+        <Navbar.Brand href="/">6B</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            
+          </Nav>
+        </Navbar.Collapse>
+    </Navbar>
+    </Container>
+  );
 }
