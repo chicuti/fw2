88f1efef25813e69c7c2fb6fcc4f4c0d524226b3
chicuti <142831353+chicuti@users.noreply.github.com>
2024-10-21 15:19:43 +0000

    aula 18-10

--- src/pages/component/navbar.js
+++ src/pages/component/navbar.js
@@ -1,20 +1,18 @@
 import {Container,Nav, Navbar} from 'react-bootstrap';
-import style from '@/styles/Navbar.module.css';
-
+import style from "@/styles/Navbar.module.css"
 export default function Menu() {
   return (
-    <Container>
-    <Navbar expand="sm" className={`${style.navGradiente} my-3 px-2 border border-2 border-success rounded rounded-pill-3`} >
+  <Container>
+    <Navbar expand="sm" className={`${style.navGradiente} px-3 my-0 border-2 border border-success rounded rounded-3`}>
         <Navbar.Brand href="/">6B</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            
+            <Nav.Link href="/noticias">Notícias</Nav.Link>
           </Nav>
         </Navbar.Collapse>
     </Navbar>
-    </Container>
+ </Container>
   );
 }
